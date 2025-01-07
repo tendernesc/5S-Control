7f78dba0736154e49066d8d2dd7cbf076221a41b
Alexandr <122694439+tendernesc@users.noreply.github.com>
2025-01-07 16:45:48 +0300

    init(5S-Control)added adaptive design to project

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Main from './pages/Main/Main';
 import Contact from './pages/Contact/Contact';
 import Tasks from './pages/Tasks/Tasks';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 export default function App() {
   return (
